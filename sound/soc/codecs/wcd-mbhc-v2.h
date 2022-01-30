0bb6b01a8b79593e6d5f61fd963ba31af4fd4525
Debayan Kar <kardebayan3@gmail.com>
2022-01-30 17:25:29 +0530

    sound: soc: codecs: Import changes from realme2pro_P-kernel-source
    Change-Id: I8310a82d0f18e3f2b6e38be7e84ca865d28bf016

--- sound/soc/codecs/wcd-mbhc-v2.h
+++ sound/soc/codecs/wcd-mbhc-v2.h
@@ -454,6 +454,12 @@ struct wcd_mbhc {
 
 	/* Work to correct accessory type */
 	struct work_struct correct_plug_swch;
+	#ifdef VENDOR_EDIT
+	/*xiang.fei@PSW.MM.AudioDriver.HeadsetDet, 2017/04/15,
+	 *Add for headset detect.
+	 */
+	struct delayed_work hp_detect_work;
+	#endif /* VENDOR_EDIT */
 	struct notifier_block nblock;
 
 	struct wcd_mbhc_register *wcd_mbhc_regs;
