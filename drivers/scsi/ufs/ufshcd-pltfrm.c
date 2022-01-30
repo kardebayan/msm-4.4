0ad433c7385dea6a3ef4b9862b33d369d865f1ac
Debayan Kar <kardebayan3@gmail.com>
2022-01-30 14:26:37 +0530

    drivers: scsi: ufs: Import changes from realme2pro_P-kernel-source
    Change-Id: Id5d4df7cc28efc52ee525b6d1eb477e6d9e83717

--- drivers/scsi/ufs/ufshcd-pltfrm.c
+++ drivers/scsi/ufs/ufshcd-pltfrm.c
@@ -398,6 +398,7 @@ int ufshcd_pltfrm_init(struct platform_device *pdev,
 	pm_runtime_set_active(&pdev->dev);
 	pm_runtime_enable(&pdev->dev);
 
+
 	return 0;
 dealloc_host:
 	ufshcd_dealloc_host(hba);
